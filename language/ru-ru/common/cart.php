2c4a11b26116a4579d5d62134f60c878dd79fdc3
Ishutin Pavel <ishutin-pavel@mail.ru>
2019-12-03 20:12:11 +0300

    Переименовать Оформить в Корзина

--- language/ru-ru/common/cart.php
+++ language/ru-ru/common/cart.php
@@ -5,4 +5,4 @@ $_['text_empty']     = 'Ваша корзина пуста!';
 $_['text_cart']      = 'Перейти в корзину';
 $_['text_checkout']  = 'Оформить заказ';
 $_['text_recurring'] = 'Платежный профиль';
-$_['text_cart_heading'] = 'Оформить';
+$_['text_cart_heading'] = 'Корзина';
